59c4560efb9c76c563686c8afaecaec07f48c3b0
Mikhail Kazannik <kazannik.m@mail.ru>
2022-08-20 23:29:29 +0300

    feat: Добавление друзей и постановка лайков. ТЗ №10 до код-ревью.

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -4,13 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
@@ -19,56 +17,11 @@ class FilmControllerTest {
     FilmController controller;
 
     @Test
-    void create() {
-        final NullPointerException nullFilmNameException = assertThrows(NullPointerException.class,
-                () -> controller.create(new Film(0,null, "Описание фильма",
-                        LocalDate.of(2022,7,31),45)));
-        Assertions.assertEquals("name is marked non-null but is null", nullFilmNameException.getMessage());
-
-        final ValidationException filmNameValidationException = assertThrows(ValidationException.class,
-                () -> controller.create(new Film(0," ", "Описание фильма",
-                LocalDate.of(2022,7,31),45)));
-        Assertions.assertEquals("Название не может быть пустым.", filmNameValidationException.getMessage());
-
-        final ValidationException filmDescriptionValidationException = assertThrows(ValidationException.class,
-                () -> controller.create(new Film(0,"Название фильма", new String(new char[201]),
-                        LocalDate.of(2022,7,31),45)));
-        Assertions.assertEquals("Максимальная длина описания фильма не может превышать 200 символов.",
-                filmDescriptionValidationException.getMessage());
-
+    void releaseDateTest() {
         final ValidationException filmReleaseDateValidationException = assertThrows(ValidationException.class,
                 () -> controller.create(new Film(0,"Название фильма", "Описание фильма",
                         LocalDate.of(1895,12,27),45)));
         Assertions.assertEquals("Дата релиза фильма не раньше 28 декабря 1895 года.",
                 filmReleaseDateValidationException.getMessage());
-
-        try {
-            controller.create(new Film(0,"Название фильма", "Описание фильма",
-                    LocalDate.of(1895,12,28),45));
-        } catch (ValidationException ex) {
-            assertNull(ex);
-        }
-
-        final ValidationException filmDurationValidationException = assertThrows(ValidationException.class,
-                () -> controller.create(new Film(0,"Название фильма", "Описание фильма",
-                        LocalDate.of(2022,7,31),0)));
-        Assertions.assertEquals("Продолжительность фильма должна быть положительной.",
-                filmDurationValidationException.getMessage());
-
-        try {
-            controller.create(new Film(1,"Название фильма", "Описание фильма",
-                    LocalDate.of(2022,7,31),1));
-        } catch (ValidationException ex) {
-            assertNull(ex);
-        }
-    }
-
-    @Test
-    void put() {
-        final NotFoundException putNewFilmValidationException = assertThrows(NotFoundException.class,
-                () -> controller.put(new Film(3,"Название фильма", "Описание фильма",
-                        LocalDate.of(2022,7,31),45)));
-        Assertions.assertEquals("Фильм (3) отсутствует в коллекции.",
-                putNewFilmValidationException.getMessage());
     }
 }
